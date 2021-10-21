732112b2f081792adb1820a10552c95c72b2fe25
casierrav <jerarquia04@gmail.com>
2021-10-20 19:19:23 -0500

    Prepare project

---
+++ src/router.js
@@ -0,0 +1,43 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import App                                    from './App.vue'
+
+import Login                                  from './components/Login.vue'
+import SignUp                                 from './components/SignUp.vue'
+import Home                                   from './components/Home.vue'
+import Account                                from './components/Account.vue'
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'root',
+    component: App
+  },
+  {
+    path: '/user/login',
+    name: "login",
+    component: Login
+  },
+  {
+    path: '/user/signUp',
+    name: "signUp",
+    component: SignUp
+  },
+  {
+    path: '/user/home',
+    name: "home",
+    component: Home
+  },
+  {
+    path: '/user/account',
+    name: "account",
+    component: Account
+  }
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
